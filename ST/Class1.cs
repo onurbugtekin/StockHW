2e8ff377f71192f749e9427573381f9eb2514161
Onur <Onur@DESKTOP-DPQN986>
2022-03-28 21:16:55 +0300

    Proje dosyası ekle.

---
+++ ST/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace AsilStok
+{ public class Stock
+    {
+        String tip;
+        decimal price;
+        decimal miktar;
+        int amount = 10;
+        public Stock(String tip) => this.tip = tip;
+
+        //public delegate void MiktarChangedHandler (object sender, EventArgs a);
+        //public event MiktarChangedHandler MiktarChanged;
+        public delegate void EHandler<TEventArgs>(object sender, TEventArgs e);
+        public event EHandler<MiktarChancedEventArgs> MiktarChanged;
+        public decimal Miktar
+        {
+            get => miktar;
+            set
+            {
+                if (miktar == amount) return;
+                decimal oldMiktar = miktar;
+                miktar = amount;
+                MiktarChanged(this, new MiktarChancedEventArgs(oldMiktar, miktar));
+            }
+        }
+        public delegate void EHandler1<TEventArgs>(object sender, TEventArgs e);
+        public event EHandler<PriceChancedEventArgs> PriceChanged;
+           public decimal Price
+        {
+            get => price;
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                PriceChanged(this, new PriceChancedEventArgs(oldPrice, price));
+            }
+        }
+      }      
+    public class MiktarChancedEventArgs : EventArgs
+    {
+        public readonly decimal oldMiktar;
+        public readonly decimal newMiktar;
+        public MiktarChancedEventArgs(decimal oldMiktar, decimal newMiktar)
+        {
+            this.oldMiktar = oldMiktar;
+            this.newMiktar = newMiktar;
+        }
+    }
+    public class PriceChancedEventArgs : EventArgs
+    {
+        public readonly decimal oldPrice;
+        public readonly decimal newPrice;
+        public PriceChancedEventArgs(decimal oldPrice, decimal newPrice)
+        {
+            this.oldPrice = oldPrice;
+            this.newPrice = newPrice;
+        }
+    }
+    
+}
---
+++ ST/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AsilStok;
+
+namespace ST
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock elma = new Stock("elma");
+            elma.PriceChanged += OnPriceChanged;
+            elma.MiktarChanged += OnMiktarChanged;
+            elma.Miktar = 0;
+            elma.Miktar = 20;
+            elma.Price = 20.4M;
+            elma.Price = 50;
+            Console.ReadKey();
+
+        }
+        static void OnMiktarChanged(object o , MiktarChancedEventArgs e)
+        {
+            Console.WriteLine("eski miktar:" + e.oldMiktar);
+            Console.WriteLine("yeni miktar:" + e.newMiktar);
+        }
+
+        static void OnPriceChanged(object o, PriceChancedEventArgs e)
+        {
+            Console.WriteLine("eski fiyat:" + e.oldPrice);
+            Console.WriteLine("yeni fiyat:" + e.newPrice);
+        }
+    }
+}
